0c886d0035cb98bfa3a85471fa445c25f8761ede
LL <liembsbs123@gmail.com>
2019-06-29 12:40:00 +0700

    chuc nang crud khuyenmai

---
+++ PTTK/Controllers/KhuyenMaiController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PTTK.Models;
+using System.Data;
+
+
+namespace PTTK.Controllers
+{
+    public class KhuyenMaiController : Controller
+    {
+        // GET: KhuyenMai
+        public ActionResult Index()
+        {
+            KhuyenMaiModel model = new KhuyenMaiModel();
+            DataTable dt = model.getAllKhuyenMai();
+            return View("Index", dt);
+        }
+
+        public ActionResult Create()
+        {
+            return View("Create");
+        }
+
+        public ActionResult InsertRecord(FormCollection form, string action)
+        {
+            if (action == "Submit")
+            {
+                KhuyenMaiModel model = new KhuyenMaiModel();
+                   
+                string loai = form["loai"];
+                string ten = form["ten"];
+                string thoiGianBatDau = form["thoiGianBatDau"];
+                string thoiGianKetThuc = form["thoiGianKetThuc"];
+                int giaTri = Convert.ToInt32(form["giaTri"]);
+                int tongTienThapNhat = Convert.ToInt32(form["tongTienThapNhat"]);
+
+                int status = model.Insert(ten, loai, thoiGianBatDau, thoiGianKetThuc, giaTri, tongTienThapNhat);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Edit(int ma)
+        {
+            KhuyenMaiModel model = new KhuyenMaiModel();
+            DataTable dt = model.getKhuyenMai(ma);
+            return View("Edit", dt);
+        }
+
+        public ActionResult UpdateRecord(FormCollection form, string action)
+        {
+            if (action == "Submit")
+            {
+                KhuyenMaiModel model = new KhuyenMaiModel();
+
+                int ma = Convert.ToInt32(form["ma"]);
+                string loai = form["loai"];
+                string ten = form["ten"];
+                string thoiGianBatDau = form["thoiGianBatDau"];
+                string thoiGianKetThuc = form["thoiGianKetThuc"];
+                int giaTri = Convert.ToInt32(form["giaTri"]);
+                int tongTienThapNhat = Convert.ToInt32(form["tongTienThapNhat"]);
+                int status = model.Update(ma, ten, loai, thoiGianBatDau, thoiGianKetThuc, giaTri, tongTienThapNhat);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+        }
+    }
+
+        public ActionResult Delete(int ma)
+        {
+            KhuyenMaiModel model = new KhuyenMaiModel();
+            model.Delete(ma);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ PTTK/Models/KhuyenMaiModel.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace PTTK.Models
+{
+    public class KhuyenMaiModel
+    {
+        string strConString = ConfigurationManager.AppSettings.Get("connString");
+
+        public DataTable getAllKhuyenMai()
+        {
+            DataTable dt = new DataTable();
+            using (SqlConnection con = new SqlConnection(strConString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Select * from KhuyenMai", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+            }
+            return dt;
+        }
+
+        public DataTable getKhuyenMai(int ma)
+        {
+            DataTable dt = new DataTable();
+            using (SqlConnection con = new SqlConnection(strConString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Select * from KhuyenMai where ma = @ma", con);
+                cmd.Parameters.AddWithValue("@ma", ma);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+            }
+            return dt;
+        }
+
+        public int Insert(string ten, string loai, string thoiGianBatDau, string thoiGianKetThuc, int giaTri, int tongTienThapNhat = 0)
+        {
+            using (SqlConnection con = new SqlConnection(strConString))
+            {
+                con.Open();
+                string query = "Insert into KhuyenMai (ten, loai,thoiGianBatDau, thoiGianKetThuc, giaTri, tongTienThapNhat ) values(@ten, @loai, @thoiGianBatDau, @thoiGianKetThuc, @giaTri, @tongTienThapNhat)";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@ten", ten);
+                cmd.Parameters.AddWithValue("@loai", loai);
+                cmd.Parameters.AddWithValue("@thoiGianBatDau", thoiGianBatDau);
+                cmd.Parameters.AddWithValue("@thoiGianKetThuc", thoiGianKetThuc);
+                cmd.Parameters.AddWithValue("@giaTri", giaTri);
+                cmd.Parameters.AddWithValue("@tongTienThapNhat", tongTienThapNhat);
+                return cmd.ExecuteNonQuery();
+            }
+        }
+
+        public int Update(int ma, string ten, string loai, string thoiGianBatDau, string thoiGianKetThuc, int giaTri, int tongTienThapNhat = 0)
+        {
+            using (SqlConnection con = new SqlConnection(strConString))
+            {
+                con.Open();
+                string query = "Update KhuyenMai SET ten = @ten, loai = @loai, thoiGianBatDau = @thoiGianBatDau, thoiGianKetThuc = @thoiGianKetThuc, giaTri = @giaTri, tongTienThapNhat = @tongTienThapNhat where ma=@ma";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@ma", ma);
+                cmd.Parameters.AddWithValue("@ten", ten);
+                cmd.Parameters.AddWithValue("@loai", loai);
+                cmd.Parameters.AddWithValue("@thoiGianBatDau", thoiGianBatDau);
+                cmd.Parameters.AddWithValue("@thoiGianKetThuc", thoiGianKetThuc);
+                cmd.Parameters.AddWithValue("@giaTri", giaTri);
+                cmd.Parameters.AddWithValue("@tongTienThapNhat", tongTienThapNhat);
+                return cmd.ExecuteNonQuery();
+            }
+        }
+
+        public int Delete(int ma)
+        {
+            using (SqlConnection con = new SqlConnection(strConString))
+            {
+                con.Open();
+                string query = "Delete KhuyenMai Where ma = @ma";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@ma", ma);
+                return cmd.ExecuteNonQuery();
+            }
+
+        }
+    }
+}
